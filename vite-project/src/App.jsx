9f85d97606cea5f6a4f074f588e671dd0c66b317
Christopher Delanoue <christopherdelanoue@Mac-Studio.local>
2025-09-26 09:50:22 +0200

    projet installé

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './assets/CSS/App.css';
+
+function App() {
+   const [count, setCount] = useState(0);
+
+   return (
+      <div className="App">
+         <h1>Hello World!</h1>
+      </div>
+   );
+}
+
+export default App;
