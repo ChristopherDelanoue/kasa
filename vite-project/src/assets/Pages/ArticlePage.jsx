d936bceea2d70f75f655c6e83058f9fc60e55c8a
Christopher Delanoue <christopherdelanoue@mac-studio.home>
2025-10-02 17:04:16 +0200

    continuation articlePage avec loader

--- vite-project/src/assets/Pages/ArticlePage.jsx
+++ vite-project/src/assets/Pages/ArticlePage.jsx
@@ -1,7 +1,31 @@
+import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import { Loader } from '../../Components/Loader.jsx';
+
 function ArticlePage() {
+   const { id } = useParams();
+   const [logement, setLogement] = useState();
+
+   useEffect(() => {
+      fetch('/Data/data.json')
+         .then((data) => data.json())
+         .then((res) => {
+            const find = res.find((a) => a.id === id);
+            setLogement(find);
+         });
+   }, [id]);
+
+   if (!logement) {
+      return <Loader />;
+   }
    return (
       <div className="ArticlePage-container">
-         <h1>Test</h1>
+         {console.log(logement)}
+         <h1>{id}</h1>
+         <img
+            src={logement.cover}
+            alt="cover"
+         />
       </div>
    );
 }
