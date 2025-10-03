6e14f78d0fe85140258864a70902a33609580393
Christopher Delanoue <christopherdelanoue@mac-studio.home>
2025-10-03 17:01:37 +0200

    sauvegarde

--- vite-project/src/assets/Pages/ArticlePage.jsx
+++ vite-project/src/assets/Pages/ArticlePage.jsx
@@ -19,39 +19,43 @@ function ArticlePage() {
    if (!logement) {
       return <Loader />;
    }
-   return (
-      <div className="ArticlePage-container">
-         {console.log(logement)}
-         <img
-            src={logement.cover}
-            alt="cover"
-         />
-         <div className="sous-photo">
-            <div className="sous-photo-gauche">
-               <div className="groupe">
-                  <div className="gauche">
-                     <h2>{logement.title}</h2>
-                     <p>{logement.location}</p>
+   if (logement) {
+      return (
+         <div className="ArticlePage-container">
+            {console.log(logement)}
+            <img
+               src={logement.cover}
+               alt="cover"
+            />
+            <div className="sous-photo">
+               <div className="sous-photo-gauche">
+                  <div className="groupe">
+                     <div className="gauche">
+                        <h2>{logement.title}</h2>
+                        <p>{logement.location}</p>
+                     </div>
+                     <div className="droite">
+                        <p>{logement.host.name}</p>
+                        <img src={logement.host.picture} />
+                     </div>
                   </div>
-                  <div className="droite">
-                     <p>{logement.host.name}</p>
-                     <img src={logement.host.picture} />
+                  <div className="groupe">
+                     <div className="tag">
+                        {logement.tags.map((tag, index) => (
+                           <p key={index}>{tag}</p>
+                        ))}
+                     </div>
+                     <div className="star"></div>
                   </div>
+                  <div className="groupe"></div>
                </div>
-               <div className="groupe">
-                  <div className="tag">
-                     {logement.tags.map((tag, index) => (
-                        <p key={index}>{tag}</p>
-                     ))}
-                  </div>
-                  <div className="star"></div>
-               </div>
-               <div className="groupe"></div>
+               <div className="sous-photo-droite"></div>
             </div>
-            <div className="sous-photo-droite"></div>
          </div>
-      </div>
-   );
+      );
+   } else {
+      return <Loader />;
+   }
 }
 
 export default ArticlePage;
